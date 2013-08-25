dbc59a37351d0556b2c60798a01ebbe5bf41f6fe
ArulKJ <arulkj07@gmail.com>
2013-08-25 12:42:15 -0400

    Removed globals and added comments to search.js

--- ClearSearch/content/search.js
+++ ClearSearch/content/search.js
@@ -1,21 +1,44 @@
-var temp = document.getElementById("searchbar");
-var searchButton = document.getAnonymousElementByAttribute(temp,"anonid", "search-go-button");
-var searchTxtBox = document.getAnonymousElementByAttribute(temp,"anonid", "searchbar-textbox");
+/* Author : Arul
+   Date : Aug 25, 2013
+   Mod 08/25 : Removed globals, added comments */
 
+/*Clear search box*/
 function clearSearch()
 {
-	if(Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch).getBoolPref("pref.privacy.clearsearch")){
-	 	searchTxtBox.value = "";
-	}
+	if(Cc["@mozilla.org/preferences-service;1"]
+		.getService(Ci.nsIPrefBranch)
+		.getBoolPref("pref.privacy.clearsearch")){
+		 	document.getAnonymousElementByAttribute(
+				document.getElementById("searchbar"),
+				"anonid",
+				"searchbar-textbox")
+			.value = "";
+		}
 }
 
-searchButton.addEventListener('click',function(){
-	clearSearch();
-});
 
-searchTxtBox.addEventListener('keypress',function(event){
-	if(event.keyCode == "13"){
+/*Eventlistener to capture search-go-button*/
+document.getAnonymousElementByAttribute(
+		document.getElementById("searchbar"),
+		"anonid",
+		"search-go-button")
+	.addEventListener('click',function(){
 		clearSearch();
 	}
-});
+);
+
+
+
+
+/* Eventlistener to capture "Enter" key-press*/
+document.getAnonymousElementByAttribute(
+		document.getElementById("searchbar"),
+		"anonid",
+		"searchbar-textbox")
+	.addEventListener('keypress',function(event){
+		if(event.keyCode == "13"){
+			clearSearch();
+		}
+	}
+);
 
